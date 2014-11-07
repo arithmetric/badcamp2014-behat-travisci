8dec264bef11e4e5e3e76530d6c9c69ae7d82c73
Greg Anderson <greg.1.anderson@greenknowe.org>
2014-11-07 15:32:22 -0800

    Sample install scripts for an existing Drupal site.

---
+++ bin/install_existing_drupal_mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SELF_DIRNAME="`dirname -- "$0"`"
+PARENT_PATH="`cd -P -- "$SELF_DIRNAME/.." && pwd -P`/`basename -- "$0"`"
+DRUPAL_ROOT="$PARENT_PATH/../drupal"
+
+#
+# This script assumes that you have customized this
+# project and moved it into your own Drupal project,
+# following the instructions in the README file.
+# You may need to edit the paths given here if
+# your layout is different than what was used in the example.
+#
+# Build an instance of Drupal using mysql.
+# This assumes that you have a mysql user
+# 'root' with no password.  Travis sets
+# up one of these for you by default.
+#
+cd "$DRUPAL_ROOT"
+drush si -y standard --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
