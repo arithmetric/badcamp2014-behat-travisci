576029d255c01a92326f2619aaefdf1c2f453628
Frank Febbraro <frankfebbraro@gmail.com>
2014-11-07 16:02:56 -0800

    fixed another drush path

--- bin/install_drupal_mysql.sh
+++ bin/install_drupal_mysql.sh
@@ -9,7 +9,7 @@
 rm -rf html
 vendor/bin/drush make -y --force-complete drupal.make html
 cd html
-vendor/bin/drush si -y standard --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
+../vendor/bin/drush si -y standard --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
 
 # We might do the same thing with drush quick-drupal
 # drush -y core-quick-drupal --profile=standard --makefile=drupal.make --db-url=mysql://root@localhost/drupal --core=drupal-7.x behat_demo devel --browser=0 --no-server --root=html --account-name=admin --account-pass=admin
